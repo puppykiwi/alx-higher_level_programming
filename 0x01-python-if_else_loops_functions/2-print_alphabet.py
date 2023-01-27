493cebba3afa0323b8b8aea8ee4ca8e9db234152
johnraymwendwa <johnraymwendwa04@gmail.com>
2023-01-27 09:11:57 +0300

    fin

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print("{}".format(chr(i)), end="")
+
+
+def print_low_alpha():
+    for c in 'abcdefghijklmnopqrstuvwxyz':
+        print('{}'.format(c), end='')
+
+if __name__ == '__main__':
+    print_low_alpha()
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    if i != 99:
-        print("{:02}".format(i), end=", ")
-    else:
-        print("{}".format(i), end="\n")
+
+
+if __name__ == '__main__':
+    for n in range(100):
+        if n != 99:
+            print('{:02d}'.format(n), end=', ')
+        else:
+            print('{:02d}'.format(n))
