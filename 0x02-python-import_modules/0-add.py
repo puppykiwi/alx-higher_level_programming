96a88f63a0ac6c4f299500ef3342e2449611eb53
johnraymwendwa <johnraymwendwa04@gmail.com>
2023-01-27 09:50:03 +0300

    namespace

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,5 @@
-from add_0 import add
-a = 5
-b = 6
-print("{} + {} = {}".format(a, b, add(a, b)), end='\n')
+if __name__ == "__main__":
+    from add_0 import add
+    a = 5
+    b = 6
+    print("{} + {} = {}".format(a, b, add(a, b)), end='\n')
