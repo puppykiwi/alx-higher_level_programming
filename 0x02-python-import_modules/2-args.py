2ababaef248437fbead6223331cb3f5f4f5e1a53
johnraymwendwa <johnraymwendwa04@gmail.com>
2023-01-27 10:58:32 +0300

    namespace

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
+if __name__ == '__main__':
+    import sys
 
-num = len(sys.argv)
-if num == 1:
-    print("0 arguments.")
-elif num == 2:
-    print("1 argument:", end='\n')
-elif num > 2:
-    print("{} arguments:".format(num), end='\n')
+    num = len(sys.argv)
+    if num == 1:
+        print("0 arguments.")
+    elif num == 2:
+        print("1 argument:", end='\n')
+    elif num > 2:
+        print("{} arguments:".format(num), end='\n')
 
-for i in range(1, num):
+    for i in range(1, num):
     print("{:d}:".format(i), sys.argv[i])
