884bb3f30e56bf874b124ea125f178a1f2661dc4
johnraymwendwa <johnraymwendwa04@gmail.com>
2023-01-27 10:33:24 +0300

    fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+
+
+if __name__ == '__main__':
     from add_0 import add
-    a = 5
-    b = 6
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)), end='\n')
+
+    a = 1
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
