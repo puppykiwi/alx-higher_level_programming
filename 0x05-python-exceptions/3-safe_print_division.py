d11c8eb71cf4f8e092905ff131f3c98db8062a53
Johnray <johnraymwendwa04@gmail.com>
2023-02-21 11:49:44 +0300

    test

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,8 +4,8 @@ def safe_print_division(a, b):
     try:
         return (a / b)
     except (NameError, ValueError,ZeroDivisionError):
-        return ("None")
+        return (None)
     finally:
-        print("{:d} / {:d} = {:d}".format(a,b,int(a/b)))
+        print("Inside result: {:d}".format(int(a/b)))
 
 # safe_print_division(8, 0)
