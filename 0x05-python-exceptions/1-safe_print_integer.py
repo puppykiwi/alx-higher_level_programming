db1de12b528bab2b943d41843d3ba85b6e69b5f6
Johnray <johnraymwendwa04@gmail.com>
2023-02-21 11:17:45 +0300

    import run prevent

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -10,3 +10,18 @@ def safe_print_integer(value):
         return True
 
 # safe_print_integer("frrg")
+if __name__ == '__main__':
+    value = 89
+    has_been_print = safe_print_integer(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = -89
+    has_been_print = safe_print_integer(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
+
+    value = "School"
+    has_been_print = safe_print_integer(value)
+    if not has_been_print:
+        print("{} is not an integer".format(value))
