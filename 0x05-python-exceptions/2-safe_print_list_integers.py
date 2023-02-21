8dacc408edd3a00378d3a8cf1bcd255201f3e08e
Johnray <johnraymwendwa04@gmail.com>
2023-02-21 11:32:27 +0300

    pycodestyle

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print('{:d}'.format(int(my_list[i])), end='')
+            count += 1
+        except (TypeError, NameError):
+            pass
+        except (IndexError):
+            break
+
+    print()
+    return count
+
+# safe_print_list_integers(my_list=[1,3,"shf",5], x=2)
