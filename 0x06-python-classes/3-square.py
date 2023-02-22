e337748313a56f97688c4df98a8d818b1ec68a9f
Johnray <johnraymwendwa04@gmail.com>
2023-02-22 12:20:06 +0300

    test

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,9 +6,9 @@ class Square:
     '''inits 2 attributes'''
     def __init__(self, size=0):
         '''initiates the atr=t'''
-        if (type(size) != int):
+        if type(size) != int:
             raise TypeError("size must be an integer.")
-        elif (size < 0):
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
