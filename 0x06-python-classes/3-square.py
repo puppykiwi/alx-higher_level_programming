af70a4c75d1dd0369ae2ee4140dde431a2ba5bc1
Johnray <johnraymwendwa04@gmail.com>
2023-02-22 12:22:20 +0300

    test

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,21 +15,3 @@ class Square:
     def area(self):
         '''this function returns the area'''
         return (self.__size * self.__size)
-
-
-if __name__ == "__main__":
-    my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
-
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
