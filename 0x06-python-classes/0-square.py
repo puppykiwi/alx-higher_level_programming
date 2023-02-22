893882f8f13f7b4f4121bdcfa027b755994e7199
Johnray <johnraymwendwa04@gmail.com>
2023-02-22 11:31:01 +0300

    test

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-class square:
+class Square:
     '''
     Creates an empty attr square
     '''
     pass
+
+if __name__ == '__main__':
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
