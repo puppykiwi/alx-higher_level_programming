57f6b481219d7754379c1d62d456d19aab644290
Johnray <johnraymwendwa04@gmail.com>
2023-02-28 10:44:49 +0300

    use __dict__

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,5 +4,18 @@
 def lookup(obj):
     '''returns a list of attributes'''
     list=[]
-    list.append(dir(obj))
+    list.append(obj.__dict__)
     return list
+
+
+if __name__ == "__main__":
+    class MyClass1(object):
+        pass
+
+    class MyClass2(object):
+        my_attr1 = 3
+        def my_meth(self):
+            pass
+
+    print(lookup(MyClass1))
+    print(lookup(MyClass2))
