72bd0d1335d435c425fd676ba3c3e1c8dd60d30c
Johnray <johnraymwendwa04@gmail.com>
2023-03-02 11:38:33 +0300

    pycodestyle

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -2,9 +2,19 @@
 '''creates a class'''
 
 
-
 class MyList(list):
     '''inherits props from list class'''
     def print_sorted(self):
-        print(self.sort)
+        print(sorted(self))
 
+
+if __name__ == "__main__":
+    my_list = MyList()
+    my_list.append(1)
+    my_list.append(4)
+    my_list.append(2)
+    my_list.append(3)
+    my_list.append(5)
+    print(my_list)
+    my_list.print_sorted()
+    print(my_list)
