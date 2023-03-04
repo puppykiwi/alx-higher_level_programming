a50f0a3f209ae30849c5a18e0ecf73fee7777568
Johnray <johnraymwendwa04@gmail.com>
2023-03-04 14:19:28 +0300

    test

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
-'''creates a class'''
 
 
 class MyList(list):
-    '''inherits props from list class'''
     def print_sorted(self):
-        print(sorted(self))
+        """Use bubble sort to create a sorted list to print
+        Args:
+            self (list): unsorted list
+        """
+        arr = self[:]
+        done = False
+        while not done:
+            done = True
+            for i in range(len(arr)-1):
+                if arr[i] > arr[i+1]:
+                    arr[i], arr[i+1] = arr[i+1], arr[i]
+                    done = False
+        print(arr)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     my_list = MyList()
     my_list.append(1)
     my_list.append(4)
