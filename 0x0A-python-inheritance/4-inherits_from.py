7802dc59daaf6c7e56cebfcd894073ff8b16fd05
Johnray <johnraymwendwa04@gmail.com>
2023-03-06 13:23:41 +0300

    added check

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,14 @@
 
 def inherits_from(obj, a_class):
     '''checks whether an object inherits from argv[1]'''
-    return issubclass(obj, a_class)
+    return isinstance(obj, a_class) if type(obj) is not a_class else False
+
+
+if __name__ == "__main__":
+    a = True
+    if inherits_from(a, int):
+        print("{} inherited from class {}".format(a, int.__name__))
+    if inherits_from(a, bool):
+        print("{} inherited from class {}".format(a, bool.__name__))
+    if inherits_from(a, object):
+        print("{} inherited from class {}".format(a, object.__name__))
