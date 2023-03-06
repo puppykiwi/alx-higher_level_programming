755c5c43e354be58b2e358a4f890f6c64b38e7d4
Johnray <johnraymwendwa04@gmail.com>
2023-03-06 13:29:38 +0300

    task6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''creates a class'''
+
+
+class BaseGeometry:
+    '''instantiates the class'''
+    def area(self):
+        '''raises an error'''
+        raise Exception("area() is not implemented")
