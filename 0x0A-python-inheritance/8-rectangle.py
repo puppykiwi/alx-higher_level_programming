3c006457ac54e52822a0ea1907920690c38ed853
Johnray <johnraymwendwa04@gmail.com>
2023-03-06 13:58:26 +0300

    task8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''creates a class'''
+
+
+class BaseGeometry:
+    '''instantiates the class'''
+    def __init__(self, width, height):
+        if type(width) != int:
+            raise TypeError("{} must be an integer".format(width))
+        if width <= 0:
+            raise ValueError("{} must be greater than 0".format(width))
+        self.__width = width
+
+        if type(height) != int:
+            raise TypeError("{} must be an integer".format(height))
+        if height <= 0:
+            raise ValueError("{} must be greater than 0".format(height))
+        self.__height = height
+
+    def area(self):
+        '''raises an error'''
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''validates input'''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
