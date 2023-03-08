57a9fe64da48e75eb21615f131ac8ddfaf67a6a2
Johnray <johnraymwendwa04@gmail.com>
2023-03-08 13:12:44 +0300

    task2

---
+++ 0x0B-python-input_output/2-append_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''appendign toa file'''
+
+
+def append_write(filename="", text=""):
+    '''opens a nd aooends text to a file'''
+    with open(filename, 'w') as f:
+        print(f.write(text), end='')
