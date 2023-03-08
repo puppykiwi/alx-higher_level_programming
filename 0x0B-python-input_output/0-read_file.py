013e3fe396607da783a3b470d7e2ae200abe5c70
Johnray <johnraymwendwa04@gmail.com>
2023-03-08 12:56:59 +0300

    use readlines()

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,4 @@
 def read_file(filename=""):
     '''opening and reading'''
     with open(filename, encoding="utf-8") as f:
-        for line in f:
-...     print(line, end='')
+        f.readlines()
