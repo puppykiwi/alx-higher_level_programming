b73e0adcc9f46c58d492e82c6ec15ef0633baf39
Johnray <johnraymwendwa04@gmail.com>
2023-03-08 12:50:10 +0300

    task0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Reading and printing a file'''
+
+
+def read_file(filename=""):
+    '''opening and reading'''
+    with open(filename, encoding="utf-8") as f:
+        f.read()
