f286b32736bf037ac3a38d0df613212693e0cfaa
Johnray <johnraymwendwa04@gmail.com>
2023-03-08 13:09:10 +0300

    use 'w' instead

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,10 @@
 
 def write_file(filename="", text=""):
     '''1wirtes to a file'''
-    with open(filename, 'r+', encodeing="utf-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
+
+
+if __name__ == "__main__":
+    nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+    print(nb_characters)
