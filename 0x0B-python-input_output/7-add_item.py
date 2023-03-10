a3da76a273249677eddc7415e411ebd53da1cdc1
Johnray <johnraymwendwa04@gmail.com>
2023-03-10 17:27:06 +0300

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,11 +3,15 @@ import json
 import sys
 '''loading lists of args'''
 
-list = []
+def additem():
+    '''just a simple function'''
+    list = []
 
-if len(sys.argv) > 1:
-    for i in range(1, len(sys.argv)):
-        list.append(sys.argv[i])
+    if len(sys.argv) > 1:
+        for i in range(1, len(sys.argv)):
+            list.append(sys.argv[i])
 
-with open("add_item.json", 'w+') as f:
-    json.dump(list, f)
+    with open("add_item.json", 'w+') as f:
+        json.dump(list, f)
+
+additem()
