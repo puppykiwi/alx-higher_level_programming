e618ca53a0690fc8e5a81b53584ba52410d5931e
Johnray <johnraymwendwa04@gmail.com>
2023-03-10 18:53:11 +0300

    task11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -2,6 +2,15 @@
 '''creating a student class'''
 
 
+def class_to_json(obj):
+    """
+    returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for JSON serialization of
+    an object
+    """
+    return obj.__dict__
+
+
 class Student:
     '''defines a student class'''
     def __init__(self, first_name, last_name, age):
