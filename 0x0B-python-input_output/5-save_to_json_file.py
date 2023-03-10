7898e80fceba1c532cbc01512721144161dfad43
Johnray <johnraymwendwa04@gmail.com>
2023-03-10 17:02:27 +0300

    task5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''loads the JSON notation to a file'''
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''opens and writes the file'''
+    with open(filename, 'w+') as f:
+        return f.write(json.dumps(my_obj))
