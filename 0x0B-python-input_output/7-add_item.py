c0df2bc036815e2b0f96a08f504b9c6194698b7b
Johnray <johnraymwendwa04@gmail.com>
2023-03-10 17:25:33 +0300

    task7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json
+import sys
+'''loading lists of args'''
+
+list = []
+
+if len(sys.argv) > 1:
+    for i in range(1, len(sys.argv)):
+        list.append(sys.argv[i])
+
+with open("add_item.json", 'w+') as f:
+    json.dump(list, f)
