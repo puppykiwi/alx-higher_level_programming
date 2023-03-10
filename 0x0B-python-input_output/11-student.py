09ffdb2bbe8427a57ecd0b223ddd522852d32f0d
Johnray <johnraymwendwa04@gmail.com>
2023-03-10 18:56:51 +0300

    taskew

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -12,14 +12,14 @@ def class_to_json(obj):
 
 
 class Student:
-    '''defines a student class'''
+    '''defines a student class that has attrs'''
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        '''returns a dict'''
+        '''returns a dict whfndevf'''
         if attrs is None:
             return self.__dict__
         else:
@@ -30,7 +30,7 @@ class Student:
             return mdict
 
     def reload_from_json(self, json):
-        '''loads from a json dict'''
+        '''loads from a json dict with alot of docs'''
         self.__dict__.clear()
         for i in json:
             self.__dict__[i] = json[i]
