db281ae84ed191cb4bc4923d25cf8a27eb887dc8
Johnray <johnraymwendwa04@gmail.com>
2023-03-10 18:50:20 +0300

    task11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+'''creating a student class'''
+
+
+class Student:
+    '''defines a student class'''
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''returns a dict'''
+        if attrs is None:
+            return self.__dict__
+        else:
+            mdict = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    mdict[i] = self.__dict__[i]
+            return mdict
+
+    def reload_from_json(self, json):
+        '''loads from a json dict'''
+        self.__dict__.clear()
+        for i in json:
+            self.__dict__[i] = json[i]
+
+
+if __name__ == "__main__":
+    import os
+    import sys
+
+    Student = __import__('11-student').Student
+    read_file = __import__('0-read_file').read_file
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file'
+                                     ).load_from_json_file
+
+    path = sys.argv[1]
+
+    if os.path.exists(path):
+        os.remove(path)
+
+    student_1 = Student("John", "Doe", 23)
+    j_student_1 = student_1.to_json()
+    print("Initial student:")
+    print(student_1)
+    print(type(student_1))
+    print(type(j_student_1))
+    print("{} {} {}".format(student_1.first_name,
+          student_1.last_name, student_1.age))
+
+    save_to_json_file(j_student_1, path)
+    read_file(path)
+    print("\nSaved to disk")
+
+    print("Fake student:")
+    new_student_1 = Student("Fake", "Fake", 89)
+    print(new_student_1)
+    print(type(new_student_1))
+    print("{} {} {}".format(new_student_1.first_name,
+          new_student_1.last_name, new_student_1.age))
+
+    print("Load dictionary from file:")
+    new_j_student_1 = load_from_json_file(path)
+
+    new_student_1.reload_from_json(j_student_1)
+    print(new_student_1)
+    print(type(new_student_1))
+    print("{} {} {}".format(new_student_1.first_name,
+          new_student_1.last_name, new_student_1.age))
