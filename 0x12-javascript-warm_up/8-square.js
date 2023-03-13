e53da1b36f4deaa759ca4e830b0bf22538936b9d
Johnray <johnraymwendwa04@gmail.com>
2023-03-13 12:26:52 +0300

    remove -

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,9 +2,9 @@
 const argv = process.argv;
 const num = parseInt(argv[2]);
 
-if (isNaN(num) || num < 0) {
+if (isNaN(num)) {
   console.log('Missing number of occurrences');
-} else {
+}else {
   for (let i = 0; i !== num; i++) {
     let s = '';
     for (let j = 0; j !== num; j++) {
