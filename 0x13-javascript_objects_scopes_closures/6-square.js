5d64fb8d852e1612e064ac5cc17ee596dc9f296a
Johnray <johnraymwendwa04@gmail.com>
2023-03-14 16:46:16 +0300

    task6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const cp = require('./5-square');
+
+class Square extends cp {
+  charPrint (c) {
+    if (c === undefined) { c = 'X'; }
+    for (let i = 0; i !== this.height; i++) {
+      let s = '';
+      for (let j = 0; j !== this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+}
