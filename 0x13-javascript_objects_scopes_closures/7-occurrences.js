5be613f1fc55136dfecabffcffdb808c761b23d7
Johnray <johnraymwendwa04@gmail.com>
2023-03-14 16:55:31 +0300

    length is a method

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 
 exports.nbOccurences = function (list, searchElement) {
   let num = 0;
-  for (let i = 0; i != list.length(); i++) {
+  for (let i = 0; i != list.length; i++) {
     if (list[i] === searchElement) {
       num += 1;
     }
