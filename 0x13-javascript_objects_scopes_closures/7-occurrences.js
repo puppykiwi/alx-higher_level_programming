5eb6c7d1ceb2878125103931cca99cab834d8d09
Johnray <johnraymwendwa04@gmail.com>
2023-03-14 16:58:46 +0300

    no letting in a loop

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,8 @@
 
 exports.nbOccurences = function (list, searchElement) {
   let num = 0;
-  for (let i = 0; i != list.length; i++) {
+  let i;
+  for (i = 0; i != list.length; i++) {
     if (list[i] === searchElement) {
       num += 1;
     }
