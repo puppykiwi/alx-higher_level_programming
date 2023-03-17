3215175f506bfaf1103a32c867b2ac40efb956eb
Johnray <johnraymwendwa04@gmail.com>
2023-03-17 13:27:46 +0300

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,6 +6,7 @@ from base import Base
 class Rectangle(Base):
     '''The rectangle class'''
     def __init__(self, height, width, x=0, y=0, id=None):
+        '''class initializer'''
         super().__init__(id)
         self.__height = height
         self.__width = width
@@ -13,34 +14,43 @@ class Rectangle(Base):
         self.__y = y
 
     @property
+    '''This is an attr getter'''
     def height(self):
         return self.__height
 
     @property
+    '''This is an attr getter'''
     def width(self):
         return self.__width
 
     @property
+    '''This is an attr getter'''
     def x(self):
         return self.__x
 
     @property
+    '''This is an attr getter'''
     def y(self):
         return self.__y
 
     @height.setter
+    '''THis is a setter'''
     def height(self, val):
         self.__height = val
 
     @width.setter
+    '''THis is a setter'''
     def width(self, val):
         self.__width = val
+        '''This is an attr setter'''
 
     @x.setter
+    '''THis is a setter'''
     def x(self, val):
         self.__x = val
 
     @y.setter
+    '''THis is a setter'''
     def y(self, val):
         self.__y = val
 
