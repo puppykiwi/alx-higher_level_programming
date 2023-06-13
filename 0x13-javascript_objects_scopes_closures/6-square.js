0244bd6b5317aae2986b56a71c9e31ffc784cace
puppykiwi <johnraymwendwa04@gmail.com>
2023-06-13 12:56:47 +0300

    inherits from rectangle

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const Square = require('./5-square');
-class Square extends Square{
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle{
     constructor(size){
         super(size, size);
     }
