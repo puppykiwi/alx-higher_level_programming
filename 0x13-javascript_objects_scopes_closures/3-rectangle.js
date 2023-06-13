df2849b347f11c340246dc077e73cdfa9840c16d
puppykiwi <johnraymwendwa04@gmail.com>
2023-06-13 12:19:56 +0300

    test

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -11,7 +11,7 @@ class Rectangle {
     for (let i = 0; i < this.height; i++) {
       let str = '';
       for (let j = 0; j < this.width; j++) {
-        str += 'x';
+        str += 'X';
       }
       console.log(str);
     }
