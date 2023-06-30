34842793fbe36b4c7cade480e006deb9b0aa51d7
puppykiwi <johnraymwendwa04@gmail.com>
2023-06-30 15:13:05 +0300

    task1 fix1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the body of the response only if the status code is 200
-curl -sL "$1"
+# Function to send GET request and display response body
+send_get_request() {
+  url=$1
+  response=$(curl -s -w "%{http_code}" "$url")
+  status_code=${response: -3}  # Extract last 3 characters (status code)
+  body=${response::-3}  # Extract response body (excluding status code)
+
+  if [[ $status_code -eq 200 ]]; then
+    echo "Response Body:"
+    echo "$body"
+  else
+    echo "Error: Status code $status_code received"
+  fi
+}
+
+# Check if URL argument is provided
+if [[ $# -eq 0 ]]; then
+  echo "Error: URL argument is required"
+  exit 1
+fi
+
+# Extract URL from the first argument
+url=$1
+
+# Call the function to send the GET request and display the response
+send_get_request "$url"
