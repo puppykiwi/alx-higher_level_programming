45d9cca1c01c1e768947f36bfd7c674bf158cdba
puppykiwi <johnraymwendwa04@gmail.com>
2023-06-30 23:12:02 +0300

    task2 fix7

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL, and displays the body of the response only if the status code is 200
-curl -sL "$1"
+curl -sL -w "%{http_code}" "$1" > response; status_code=${response: -3}; body=${response::-3}; if [[ $status_code -eq 200 ]]; then echo "$body"; fi
