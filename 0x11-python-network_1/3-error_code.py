0f2f2deddb1b7f506c78fb966e8ba281eb7880ff
puppykiwi <johnraymwendwa04@gmail.com>
2023-06-30 09:48:11 +0300

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''handling http errors'''
+from urllib.request import urlopen as open
+from urllib.error import HTTPError
+from sys import argv
+url = argv[1]
+
+if __name__ == "__main__":
+    try:
+        with open(url) as response:
+            data = response.read().decode('utf-8')
+            print(data)
+    except HTTPError as e:
+        print('Error code: {}'.format(e.code))
