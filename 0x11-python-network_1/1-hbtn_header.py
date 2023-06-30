1e1269d2fe47c02002feb7ba94bffb376f163223
puppykiwi <johnraymwendwa04@gmail.com>
2023-06-30 14:33:02 +0300

    task1 fix2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-'''reading http headers using urllib package'''
+"""reading http headers using urllib package"""
 from urllib.request import urlopen as open
 from sys import argv
-url = argv[1]
 
 if __name__ == "__main__":
+    url = argv[1]
     with open(url) as response:
         print(response.headers.get('X-Request-Id'))
