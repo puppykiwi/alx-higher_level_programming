6ad7e319d320ce783555b532a305d921e1c1cd1a
puppykiwi <johnraymwendwa04@gmail.com>
2023-06-30 23:25:51 +0300

    task5 fix0

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a post request with variables set and display body of response
-curl -sX post -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=hr@holbertonschool.com" -d "subject=I will always be here for PLD" "$1"
