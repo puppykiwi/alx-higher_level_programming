b2b232b83b38cfa2f04e868c039dbf9a21b0e26f
puppykiwi <johnraymwendwa04@gmail.com>
2023-06-30 14:21:26 +0300

    task 0 fix 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ with uopen('https://alx-intranet.hbtn.io/status') as response:
     html = response.read()
 
 if __name__ == "__main__":
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    print("Body response:$")
+    print("\t- type: {}$".format(type(html)))
+    print("\t- content: {}$".format(html))
+    print("\t- utf8 content: {}$".format(html.decode('utf-8')))
