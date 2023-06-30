cb074e2f70ad8ca9a45d403c248648d48bb5aec3
puppykiwi <johnraymwendwa04@gmail.com>
2023-06-30 22:59:28 +0300

    task2 fix0

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send a delete request
-response=$(curl -sL -X DELETE "$1");echo "$response"; body=${response::-3}; echo "$body"
+response=$(curl -sL -X DELETE "$1"); body=$response; echo "$body"
