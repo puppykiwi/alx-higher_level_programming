f440909972b41d1e7b9d8764addefe9a2f524deb
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-25 12:19:07 +0300

    task 1 fix 2

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,8 +4,6 @@
  * If no error occurs, prints 'The file was saved!'
 */
 const fs = require('fs');
-const file = process.argv[2];
-const string = process.argv[3];
 
 fs.writeFile(process.argv[2], process.argv[3], error => {
   if (error) console.log(error);
