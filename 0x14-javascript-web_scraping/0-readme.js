6af87ffbb521c73d00701dea8cf2778090d4eaba
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-25 11:04:50 +0300

    task 0 fix SS

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,8 @@
 const fs = require('fs');
 const file = process.argv[2];
 fs.readFile(file, 'utf8', function (err, data) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(data);
-    });
-
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
