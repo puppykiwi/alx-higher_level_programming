d987320fb890d43bcedf6d7b6640ad9e53f52567
puppykiwi <johnraymwendwa04@gmail.com>
2023-07-25 12:03:46 +0300

    task 1 fix 0

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,6 +6,7 @@
 const fs = require('fs');
 const file = process.argv[2];
 const string = process.argv[3];
+
 fs.writeFile(file, string, 'utf8', function (err) {
   if (err) {
     return console.log(err);
