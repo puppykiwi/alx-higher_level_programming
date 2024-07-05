0f741ea79588bf1f4d4de753394f05e7e7dd1db6
Johnray Mwendwa <johnraymwendwa04@gmail.com>
2024-07-05 10:50:25 +0300

    pep8

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,7 +9,7 @@ class Base:
     def __init__(self, id=None):
         '''class initiator'''
         if id is not None:
-            self.id = id 
+            self.id = id
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
