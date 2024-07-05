1b23812a226ae49afe3aa44a97f6fa50c41c00f7
Johnray Mwendwa <johnraymwendwa04@gmail.com>
2024-07-05 11:01:48 +0300

    new

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-'''Creates a rectangle class'''
+"""Defines a rectangle class."""
 from models.base import Base
 
 
 class Rectangle(Base):
-    '''The rectangle class'''
-    def __init__(self, height, width, x=0, y=0, id=None):
+    """Represent a rectangle."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle.
         Args:
             width (int): The width of the new Rectangle.
@@ -19,62 +20,137 @@ class Rectangle(Base):
             TypeError: If either of x or y is not an int.
             ValueError: If either of x or y < 0.
         """
-        '''class initializer'''
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
-        self.__height = height
-        self.__width = width
-        self.__x = x
-        self.__y = y
-
-    @property
-    def height(self):
-        '''This is an attr height getter'''
-        return self.__height
 
     @property
     def width(self):
-        '''This is an attr width getter'''
+        """Set/get the width of the Rectangle."""
         return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Set/get the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
 
     @property
     def x(self):
-        '''This is an attr y getter'''
+        """Set/get the x coordinate of the Rectangle."""
         return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
 
     @property
     def y(self):
-        '''This is an attr x getter'''
+        """Set/get the y coordinate of the Rectangle."""
         return self.__y
 
-    @height.setter
-    def height(self, val):
-        '''THis is a height setter'''
-        self.__height = val
-
-    @width.setter
-    def width(self, val):
-        '''THis is a width setter'''
-        self.__width = val
-
-    @x.setter
-    def x(self, val):
-        '''THis is a x setter'''
-        self.__x = val
-
     @y.setter
-    def y(self, val):
-        '''THis is a y setter'''
-        self.__y = val
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
 
+    def area(self):
+        """Return the area of the Rectangle."""
+        return self.width * self.height
 
-if __name__ == "__main__":
+    def display(self):
+        """Print the Rectangle using the `#` character."""
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
 
-    r1 = Rectangle(10, 2)
-    print(r1.id)
+        [print("") for y in range(self.y)]
+        for h in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
+            print("")
 
-    r2 = Rectangle(2, 10)
-    print(r2.id)
+    def update(self, *args, **kwargs):
+        """Update the Rectangle.
+        Args:
+            *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents width attribute
+                - 3rd argument represent height attribute
+                - 4th argument represents x attribute
+                - 5th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
 
-    r3 = Rectangle(10, 3, 0, 0, 12)
-    print(r3.id)
-    print(r3.area())
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
+    def to_dictionary(self):
+        """Return the dictionary representation of a Rectangle."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        """Return the print() and str() representation of the Rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
